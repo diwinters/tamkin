b7fd9e95b4d0c9547857a64b7dc3f906fa76bb9a
diwinters <elmokhtar.cheikh@gmail.com>
2025-02-22 15:17:39 +0100

    edit tagsinput.tsx

--- src/components/PostSubmissionEditor/TagsInput.tsx
+++ src/components/PostSubmissionEditor/TagsInput.tsx
@@ -8,8 +8,7 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 import { NC_SITE_SETTINGS } from '@/contains/site-settings'
 import getTrans from '@/utils/getTrans'
 
-const MAX_TAGS_LENGTH =
-	NC_SITE_SETTINGS['submissions-settings']?.max_tags_allowed || 5
+const MAX_TAGS_LENGTH = NC_SITE_SETTINGS['submissions-settings']?.max_tags_allowed || 5
 
 export interface TagNodeShort extends NcmazFcTagShortFieldsFragmentFragment {}
 
@@ -21,18 +20,15 @@ interface TagsInputProps {
 const TagsInput: FC<TagsInputProps> = ({ onChange, defaultValue }) => {
 	const T = getTrans()
 
-	const [queryGetTags, { loading, error, data, fetchMore, called }] =
-		useLazyQuery(QUERY_GET_TAGS, {
-			notifyOnNetworkStatusChange: true,
-			context: {
-				fetchOptions: {
-					method: process.env.NEXT_PUBLIC_SITE_API_METHOD || 'GET',
-				},
+	const [queryGetTags, { loading, error, data, fetchMore, called }] = useLazyQuery(QUERY_GET_TAGS, {
+		notifyOnNetworkStatusChange: true,
+		context: {
+			fetchOptions: {
+				method: process.env.NEXT_PUBLIC_SITE_API_METHOD || 'GET',
 			},
-			variables: {
-				first: 50,
-			},
-		})
+		},
+		variables: { first: 50 },
+	})
 
 	const handleClickShowMore = () => {
 		fetchMore({
@@ -43,43 +39,32 @@ const TagsInput: FC<TagsInputProps> = ({ onChange, defaultValue }) => {
 				if (!fetchMoreResult || !fetchMoreResult?.tags?.nodes) {
 					return prev
 				}
-
 				return {
 					tags: {
 						...fetchMoreResult.tags,
-						nodes: [
-							...(prev?.tags?.nodes || []),
-							...fetchMoreResult.tags.nodes,
-						],
+						nodes: [...(prev?.tags?.nodes || []), ...fetchMoreResult.tags.nodes],
 					},
 				}
 			},
 		})
 	}
 
-	//
-	const [inputTextValue, setInputTextValue] = useState('')
-	//
 	const containerRef = useRef<HTMLDivElement>(null)
-	const inputRef = useRef<HTMLInputElement>(null)
-
-	let [isOpen, setIsOpen] = useState(false)
+	const [isOpen, setIsOpen] = useState(false)
 	const [tags, setTags] = useState<TagNodeShort[]>(defaultValue || [])
 
 	useEffect(() => {
-		if (!isOpen) {
-			return
+		if (isOpen) {
+			queryGetTags()
 		}
-		queryGetTags()
 	}, [isOpen])
 
 	useEffect(() => {
 		if (tags.length >= MAX_TAGS_LENGTH) {
 			setIsOpen(false)
 		}
-
 		onChange(tags)
-	}, [tags.length])
+	}, [tags])
 
 	function closePopover() {
 		setIsOpen(false)
@@ -89,65 +74,25 @@ const TagsInput: FC<TagsInputProps> = ({ onChange, defaultValue }) => {
 		setIsOpen(true)
 	}
 
-	const checkIncludes = (tag: TagNodeShort) => {
-		return tags.some((item) => item.name === tag.name)
-	}
+	const checkIncludes = (tag: TagNodeShort) => tags.some((item) => item.name === tag.name)
 
-	const setNewTags = (tag: TagNodeShort) => {
-		if (!checkIncludes(tag)) {
-			setTags((prevTags) => [...prevTags, tag])
+	const handleAddTag = (tag: TagNodeShort) => {
+		if (!checkIncludes(tag) && tags.length < MAX_TAGS_LENGTH) {
+			setTags([...tags, tag])
 		}
-		setInputTextValue('')
-		inputRef.current?.focus()
-	}
-
-	useEffect(() => {
-		if (eventClickOutsideDiv) {
-			document.removeEventListener('click', eventClickOutsideDiv)
-		}
-		isOpen && document.addEventListener('click', eventClickOutsideDiv)
-		return () => {
-			document.removeEventListener('click', eventClickOutsideDiv)
-		}
-	}, [isOpen])
-
-	const eventClickOutsideDiv = (event: MouseEvent) => {
-		if (!isOpen || !containerRef.current) {
-			return
-		}
-
-		// CLICK IN_SIDE
-		if (
-			containerRef.current.contains(event.target as Node) ||
-			inputRef.current?.contains(event.target as Node)
-		) {
-			return
-		}
-
-		// CLICK OUT_SIDE
-		setIsOpen(false)
 	}
 
 	const handleRemoveTag = (tag: TagNodeShort) => {
 		setTags(tags.filter((t) => t.name !== tag.name))
-		setTimeout(() => {
-			inputRef.current?.focus()
-		}, 100)
 	}
 
-	const tagsData = (data?.tags?.nodes ||
-		[]) as NcmazFcTagShortFieldsFragmentFragment[]
-
-	const isMax = tags.length >= MAX_TAGS_LENGTH
+	const tagsData = (data?.tags?.nodes || []) as NcmazFcTagShortFieldsFragmentFragment[]
 
 	return (
 		<div className="relative w-full text-xs sm:text-sm">
 			<ul className="flex flex-wrap gap-1.5">
 				{tags.map((tag) => (
-					<li
-						className="flex items-center justify-center rounded-lg bg-neutral-100 px-3 py-2 dark:bg-neutral-800"
-						key={tag.databaseId}
-					>
+					<li key={tag.databaseId} className="flex items-center rounded-lg bg-neutral-100 px-3 py-2 dark:bg-neutral-800">
 						# {tag.name}
 						<button
 							className="ms-1 flex items-center justify-center px-1 text-base hover:text-neutral-900 dark:hover:text-neutral-50"
@@ -159,73 +104,22 @@ const TagsInput: FC<TagsInputProps> = ({ onChange, defaultValue }) => {
 					</li>
 				))}
 
-				{!isMax && (
+				{tags.length < MAX_TAGS_LENGTH && (
 					<li>
-						{/* <Popover.Button>Solutions</Popover.Button> */}
-						<input
-							ref={inputRef}
-							value={inputTextValue}
-							onChange={(e) => {
-								const regex = /^[a-zA-Z0-9]*$/ // Cho phép chỉ nhập các ký tự chữ và số
-								const value = e.target.value
-								const lastChar = value[value.length - 1]
-
-								if (
-									(lastChar === ' ' || lastChar === ',') &&
-									value.length > 1
-								) {
-									setNewTags({
-										databaseId: Date.now(),
-										name: value.replace(/[, ]/g, ''),
-										__typename: 'Tag',
-									})
-									return
-								}
-
-								if (regex.test(value)) {
-									setInputTextValue(value)
-								}
-							}}
-							className={`${
-								tags.length ? 'px-3' : 'px-0'
-							} h-full border-none bg-transparent py-2 text-base text-neutral-600 hover:text-neutral-900 focus:outline-none focus:ring-0 disabled:cursor-not-allowed dark:text-neutral-400 dark:hover:text-neutral-300`}
-							type="text"
-							placeholder={
-								!tags.length
-									? `${T.pageSubmission['Add tags']} (${tags.length}/${MAX_TAGS_LENGTH})...`
-									: `${T.pageSubmission['Add tag']} (${tags.length}/${MAX_TAGS_LENGTH})`
-							}
-							onFocus={openPopover}
-							onKeyUp={(e) => {
-								// check if the key is not Enter, Space, Comma
-								if (e.key !== 'Enter' && e.key !== 'Tab') {
-									return
-								}
-								setNewTags({
-									databaseId: Date.now(),
-									name: e.currentTarget.value.replace(/[, ]/g, ''),
-									__typename: 'Tag',
-								})
-							}}
-						/>
+						<button onClick={openPopover} className="px-3 py-2 text-neutral-600 dark:text-neutral-400">
+							+ {T.pageSubmission['Add tag']}
+						</button>
 					</li>
 				)}
 			</ul>
-			<i className="my-1 text-[10px] sm:hidden">
-				(For add a tag, press the <code>"Enter"</code>, <code>"Space"</code> or{' '}
-				<code>","</code> key after typing.)
-			</i>
 
 			{isOpen && (
-				<div
-					ref={containerRef}
-					className="absolute inset-x-0 top-full z-50 mt-4 space-y-5 rounded-2xl bg-white p-5 shadow-lg ring-1 ring-black/[0.03] dark:bg-neutral-800"
-				>
+				<div ref={containerRef} className="absolute inset-x-0 top-full z-50 mt-4 rounded-2xl bg-white p-5 shadow-lg dark:bg-neutral-800">
 					<h3 className="text-xl font-semibold">{T.Tags}</h3>
-					<div className="w-full border-b border-neutral-300 dark:border-neutral-700" />
+					<div className="border-b border-neutral-300 dark:border-neutral-700" />
 					{!!error && <p className="text-red-500">{error.message}</p>}
 					{!!loading && !tagsData.length && <Loading />}
-					{!!tagsData.length ? (
+					{!!tagsData.length && (
 						<ul className="flex flex-wrap gap-2">
 							{tagsData.map((tag) => {
 								const isSelected = checkIncludes(tag)
@@ -240,31 +134,29 @@ const TagsInput: FC<TagsInputProps> = ({ onChange, defaultValue }) => {
 											onClick={() => {
 												if (isSelected) {
 													handleRemoveTag(tag)
-													return
+												} else {
+													handleAddTag(tag)
 												}
-												!isMax && setNewTags(tag)
 											}}
 										>
-											#{` `}
-											{tag.name} ({tag.count || 0})
+											#{tag.name} ({tag.count || 0})
 										</button>
 									</li>
 								)
 							})}
 						</ul>
-					) : null}
+					)}
 
-					{/* SHOW MORE */}
-					{data?.tags?.pageInfo.hasNextPage ? (
+					{data?.tags?.pageInfo.hasNextPage && (
 						<>
-							<div className="w-full border-b border-neutral-300 dark:border-neutral-700" />
+							<div className="border-b border-neutral-300 dark:border-neutral-700" />
 							<div className="flex justify-center">
 								<ButtonPrimary loading={loading} onClick={handleClickShowMore}>
 									{T.pageSubmission['Load more tags']}
 								</ButtonPrimary>
 							</div>
 						</>
-					) : null}
+					)}
 				</div>
 			)}
 		</div>
