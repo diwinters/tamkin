a80cc36c69bed240eda77ba3f1cbb160635c642c
diwinters <elmokhtar.cheikh@gmail.com>
2025-02-22 14:44:13 +0100

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,54 +7,56 @@ import '@/styles/index.scss'
 import { AppProps } from 'next/app'
 import { WordPressBlocksProvider, fromThemeJson } from '@faustwp/blocks'
 import blocks from '@/wp-blocks'
-import { Lateef } from 'next/font/google'
+import { Noto_Kufi_Arabic } from 'next/font/google'
 import SiteWrapperProvider from '@/container/SiteWrapperProvider'
 import { Toaster } from 'react-hot-toast'
 import NextNProgress from 'nextjs-progressbar'
 import themeJson from '@/../theme.json'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
 
-const lateefFont = Lateef({  // Renamed to avoid conflict
-	subsets: ['latin'],
-	display: 'swap',
-	weight: ['300', '400', '500', '600', '700'],
+// Import the font
+const notoKufiArabic = Noto_Kufi_Arabic({
+  subsets: ['arabic'],
+  display: 'swap',
+  weight: ['400', '500', '600', '700'],
 })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	const router = useRouter()
+  const router = useRouter()
 
-	return (
-		<>
-			<GoogleAnalytics trackPageViews />
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
 
-			<FaustProvider pageProps={pageProps}>
-				<WordPressBlocksProvider
-					config={{
-						blocks,
-						theme: fromThemeJson(themeJson),
-					}}
-				>
-					<SiteWrapperProvider {...pageProps}>
-						<style jsx global>{`
-							html {
-								font-family: ${lateefFont.style.fontFamily};
-							}
-						`}</style>
-						<NextNProgress color="#818cf8" />
-						<Component {...pageProps} key={router.asPath} />
-						<Toaster
-							position="bottom-left"
-							toastOptions={{
-								style: {
-									fontSize: '14px',
-									borderRadius: '0.75rem',
-								},
-							}}
-							containerClassName="text-sm"
-						/>
-					</SiteWrapperProvider>
-				</WordPressBlocksProvider>
-			</FaustProvider>
-		</>
-	)
+      <FaustProvider pageProps={pageProps}>
+        <WordPressBlocksProvider
+          config={{
+            blocks,
+            theme: fromThemeJson(themeJson),
+          }}
+        >
+          <SiteWrapperProvider {...pageProps}>
+            {/* Apply the font globally */}
+            <style jsx global>{`
+              html {
+                font-family: ${notoKufiArabic.style.fontFamily};
+              }
+            `}</style>
+            <NextNProgress color="#818cf8" />
+            <Component {...pageProps} key={router.asPath} />
+            <Toaster
+              position="bottom-left"
+              toastOptions={{
+                style: {
+                  fontSize: '14px',
+                  borderRadius: '0.75rem',
+                },
+              }}
+              containerClassName="text-sm"
+            />
+          </SiteWrapperProvider>
+        </WordPressBlocksProvider>
+      </FaustProvider>
+    </>
+  )
 }
